f893d93ab248532ceb8dffba38d8b58d477e69dd
Johnson-olaolu <johnsonolaolu@gmail.com>
2022-04-04 05:16:40 +0100

    fixed migration and seeding

--- seeders/20220211234607-seedRoles.js
+++ seeders/20220211234607-seedRoles.js
@@ -1,27 +1,32 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Roles', [
       {
-      name: 'admin',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      name: 'customer',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      name: 'vetter',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }
-  ]);
+        name: 'admin',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'customer',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'vetter',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'super-admin',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     return queryInterface.bulkDelete('Role', null, {});
   }
 };
